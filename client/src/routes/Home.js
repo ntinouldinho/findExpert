7626cbf38c2506b2ca83914eaf3a9f8a92cf7af6
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2021-04-07 23:13:35 +0300

    create login button and redirect

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
+import { useHistory } from 'react-router-dom';
 import { HomeCategory } from "../components/HomeCategory";
 import  SearchBar  from "../components/SearchBar.js";
 import '../CSS/Home.css';
 import logo from "../assets/logo.png";
 
+const Login = () => {
+
+    const history = useHistory();
+    const toLogin = (e) => {
+        history.push(`/login`);
+        e.preventDefault();
+    };
+
+    return (
+        <input type="button" onClick={toLogin} id="login" value="Login/Register"/>
+    );
+};
+
+
+
 export class Home extends Component { 
     constructor(props){
         super(props);
         this.state = {
-            src: ['Mechanic','Mechanic','Mechanic'],
+            src: ['Mechanic','Doctor','Chef'],
             search: ""
         };
     }
-    
+
     render(){
         return(
             <div >
                 <img src={logo} alt="logo" height="100" width="200"/>
+                <Login />
                 <SearchBar 
                     value={this.state.search}
                     handleChange={(e)=> this.setState({search:e.target.value})} 
