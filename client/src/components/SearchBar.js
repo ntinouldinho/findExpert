cd53fe429ea94dfe3673b9cedc453ea05422345f
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2021-04-07 22:47:34 +0300

    Create SearchBar.js

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,37 @@
+import React from 'react';  
+import { useHistory } from 'react-router-dom';
+import '../CSS/Home.css';
+
+
+const SearchBar = (props) => {
+    const history = useHistory();
+    const onSubmit = (e) => {
+        history.push(`/search/${props.value}`);
+        e.preventDefault();
+    };
+
+    return (
+        <form
+            action="/"
+            method="get"
+            autoComplete="off"
+            onSubmit={onSubmit}
+        >
+            <label htmlFor="search">
+                   {props.placeholder}
+            </label>
+            <input
+                type="search"
+                placeholder={props.placeholder}
+                nane="search"
+                onChange={props.handleChange}
+            />
+            <button type="submit">Search</button>
+        </form>
+    );
+};
+
+export default SearchBar;
+
+
+
