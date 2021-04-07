f090850ae64808ba3b9cbf49925bf40ca3815fdc
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2021-04-07 22:48:19 +0300

    add search bar

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,19 +1,33 @@
-import React from 'react';
-// import HomeCategory from '../components/HomeCategory';
+import React, { Component } from 'react';
 import { HomeCategory } from "../components/HomeCategory";
+import  SearchBar  from "../components/SearchBar.js";
 import '../CSS/Home.css';
+import logo from "../assets/logo.png";
 
-function Home(){
+export class Home extends Component { 
+    constructor(props){
+        super(props);
+        this.state = {
+            src: ['Mechanic','Mechanic','Mechanic'],
+            search: ""
+        };
+    }
+    
+    render(){
+        return(
+            <div >
+                <img src={logo} alt="logo" height="100" width="200"/>
+                <SearchBar 
+                    value={this.state.search}
+                    handleChange={(e)=> this.setState({search:e.target.value})} 
+                    placeholder="Search for category" />
 
-    return(
-        <div className="HomeCategories">
-            <HomeCategory name = "Mechanic"/>
-            <HomeCategory name = "Mechanic"/>
-            <HomeCategory name = "Mechanic"/>
-        </div>
-    );
-        
+                <div className="HomeCategories">{this.state.src.map((item,i) =>  <HomeCategory key={i} name = {item}/>)}</div>
+                
+            </div>
+        );
+         
+    }
+       
     
 }
-
-export default Home;
