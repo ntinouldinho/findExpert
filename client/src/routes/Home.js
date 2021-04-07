857aa7583eb5a71fd471dc3ff907c0e7e9d1cb26
Bill Ballas <ballas@programmer.net>
2021-04-08 02:02:40 +0300

    home css changes

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -1,50 +1,55 @@
-import React, { Component } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { Component } from "react";
+import { useHistory } from "react-router-dom";
 import { HomeCategory } from "../components/HomeCategory";
-import  SearchBar  from "../components/SearchBar.js";
-import '../CSS/Home.css';
+import SearchBar from "../components/SearchBar.js";
+import "../CSS/Home.css";
 import logo from "../assets/logo.png";
 
 const Login = () => {
+  const history = useHistory();
+  const toLogin = (e) => {
+    history.push(`/login`);
+    e.preventDefault();
+  };
 
-    const history = useHistory();
-    const toLogin = (e) => {
-        history.push(`/login`);
-        e.preventDefault();
-    };
-
-    return (
-        <input type="button" onClick={toLogin} id="login" value="Login/Register"/>
-    );
+  return (
+    <input type="button" onClick={toLogin} id="login" value="Login/Register" />
+  );
 };
 
+export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      src: ["Mechanic", "Doctor", "Chef"],
+      search: "",
+    };
+  }
 
-
-export class Home extends Component { 
-    constructor(props){
-        super(props);
-        this.state = {
-            src: ['Mechanic','Doctor','Chef'],
-            search: ""
-        };
-    }
-
-    render(){
-        return(
-            <div >
-                <img src={logo} alt="logo" height="100" width="200"/>
-                <Login />
-                <SearchBar 
-                    value={this.state.search}
-                    handleChange={(e)=> this.setState({search:e.target.value})} 
-                    placeholder="Search for category" />
-
-                <div className="HomeCategories">{this.state.src.map((item,i) =>  <HomeCategory key={i} name = {item}/>)}</div>
-                
-            </div>
-        );
-         
-    }
-       
-    
+  render() {
+    return (
+      <div>
+        <div className="logoSearch">
+          <img
+            className="Logo"
+            src={logo}
+            alt="logo"
+            height="200"
+            width="400"
+          />
+          <Login />
+          <SearchBar
+            value={this.state.search}
+            handleChange={(e) => this.setState({ search: e.target.value })}
+            placeholder="Search for category"
+          />
+        </div>
+        <div className="HomeCategories">
+          {this.state.src.map((item, i) => (
+            <HomeCategory key={i} name={item} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
