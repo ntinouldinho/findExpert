aac8fb32c23fa849dc7d2c72035d04e1bc321c97
GerasimosLap <gerasimoslapakis@gmail.com>
2021-04-08 19:13:23 +0300

    GRID

--- client/src/routes/Profile.js
+++ client/src/routes/Profile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../CSS/Profile.css';
 import logo from "../assets/logo.png";
 import ProfessionalBlock from '../components/ProfessionalBlock.js';
+import StarRatingComponent from 'react-star-rating-component';
 
 export class Profile extends Component { 
     constructor(props){
@@ -15,49 +16,48 @@ export class Profile extends Component {
             cv: "My CV",
             about: "About me",
             stars: "5 STARS",
-            contact: "message",
             appointment: "appointment"
         };
     }
 
     render(){
         return(
-                <div>
-                    <div className="header">
+            <div> 
+                <div className="header">
+                    <div className = "grid-container" style={{margin: "15%" }}>
                         <div className="picture">
                             <img src={"https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/car_mechanic.jpg?alt=media&token=23cb2e86-4440-404d-9fdc-53e99eb2a2dc"} alt="Profile Picture" height="100" width="200" id="profpic"/>
                             {/* <img src={logo} alt="Profile Picture" height="100" width="200" id="profpic"/> */}
                         </div>
+
                         <div className="info">
-                            <ProfessionalBlock person={{name:this.state.name,job:this.state.profession}} />
+                            {this.state.name}<br></br>
+                            {this.state.profession}<br></br>
+                            {this.state.stars}<br></br>
                         </div>
+
                         <div className="aboutme">
                             {this.state.about}
                         </div>
-                    </div>
 
-                    <div className="header">
                         <div className="services">
-                            {this.state.services}<br></br>
+                                {this.state.services}<br></br>
                         </div>
+
                         <div className="appointment">
-                            {this.state.appointment}<br></br>
+                                {this.state.appointment}<br></br>
                         </div>
-                        <div className="contact">
-                            {this.state.contact}<br></br>
+
+                        <div className="cv">
+                                {this.state.cv}<br></br>
+                        </div>
+
+                        <div className="reviews">
+                                {this.state.reviews}<br></br>
                         </div>
                     </div>
-                        
-                    <div className="cv"> 
-                        {this.state.cv}
-                    </div>
-
-                    <div className="reviews"> 
-                        {this.state.reviews}
-                    </div>
-
-            </div>
-        );
+                </div>
+            </div>   
+        )
     }
 }
-                     
