990ed1c63ad99453a5c6c53ec4a83536e2e7055c
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2021-04-08 23:10:18 +0300

    put new user in firebase

--- client/src/routes/Login.js
+++ client/src/routes/Login.js
@@ -1,48 +1,76 @@
 import React, { Component } from 'react';
 import '../CSS/Login.css';
 import logo from "../assets/black-logo.png";
-import profile from "../assets/blank-profile-picture.png";
 import Button from 'react-bootstrap/Button';
 
-import Rating from 'react-star-review';
-// import ReactSummernote from 'react-summernote';
-// import 'react-summernote/dist/react-summernote.css'; // import styles
-// import 'react-summernote/lang/summernote-el-GR'; // you can import any other locale
- 
-// <ReactSummernote
-// value="Default value"
-// options={{
-//   lang: 'ru-RU',
-//   height: 350,
-//   dialogsInBody: true,
-//   toolbar: [
-//     ['style', ['style']],
-//     ['font', ['bold', 'underline', 'clear']],
-//     ['fontname', ['fontname']],
-//     ['para', ['ul', 'ol', 'paragraph']],
-//     ['table', ['table']],
-//     ['insert', ['link', 'picture', 'video']],
-//     ['view', ['fullscreen', 'codeview']]
-//   ]
-// }}
-// />
+
 export class Login extends Component { 
     constructor(props){
         super(props);
         this.state = {
-            isLogin:false
+            isLogin:false,
+            name:"",
+            username:"",
+            password:""
         }
-        this.onSubmit = this.onSubmit.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.onChangeSign = this.onChangeSign.bind(this);
+
+        this.onTodoChange = this.onTodoChange.bind(this);
     }
+
+
+    onTodoChange(e) {
+        const name = e.target.name;
+        if(name=='name'){
+            this.setState({ name: e.target.value });
+        }else if(name=="password"){
+            this.setState({ password: e.target.value });
+        }else if(name=="username"){
+            this.setState({ username: e.target.value });
+        }
+        
+        console.log(this.state);
+    }
+    
+    
+
+    componentDidMount() {
+        this.callApi()
+          .then(res => console.log(res))
+          .catch(err => console.log(err));
+    }
+    
+    callApi = async () => {
+        const response = await fetch('/api/hello');
+        const body = await response.json();
+
+        if (response.status !== 200) throw Error(body.message);
+
+        return body;
+    };
 
     onChangeSign(){
         this.setState({isLogin:!this.state.isLogin});
     }
     
-    onSubmit() {
-        console.log("in");
-    }
+    handleSubmit = async e => {
+        e.preventDefault();
+        const response = await fetch('/api/addUser', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ 
+              name: this.state.name, 
+              username: this.state.username,
+              password: this.state.password
+            }),
+        });
+        
+        //const body = await response.text();
+    
+      };
 
     render(){
         const text = this.state.isLogin?"Sign In":"Sign Up";
@@ -52,12 +80,11 @@ export class Login extends Component {
             <div id="center-div">
                 <img src={logo} alt="logo"  width="45%" id="logo"/>
                 
-    <Rating rating={7.7} count={10}/>
                     <form
                         action="/"
                         method="get"
                         autoComplete="off"
-                        onSubmit={this.onSubmit}
+                        onSubmit={this.handleSubmit}
                     >
                         <h2>You want to {text}</h2>
                         <br/>
@@ -66,42 +93,29 @@ export class Login extends Component {
                         <br/><br/>
                           
 
-
-                        <div id="sign_in_div" style={{display: this.state.isLogin ? 'block' : 'none' }}>
-                            <h4>Username:</h4>
-                            <input
-                                type="text"
-                                placeholder={"Enter your username..."}
-                                name="username"
-                            />
-
-                            <br/><br/>
-
-                            <h4>Password:</h4>
-                            <input
-                                type="text"
-                                placeholder={"Enter your password..."}
-                                name="password"
-                            />
-
-                        </div>
                         
 
-                        <div id="sign_up_div" style={{display: !this.state.isLogin ? 'block' : 'none' }}>
-                            <h4>Name:</h4>
-                            <input
-                                type="text"
-                                placeholder={"Enter your name..."}
-                                name="name"
-                            />
+                        <div id="sign_up_div">
 
-                            <br/><br/>
+                            <div  style={{display: !this.state.isLogin ? 'block' : 'none' }}>
+                                <h4>Name:</h4>
+                                <input
+                                    type="text"
+                                    placeholder={"Enter your name..."}
+                                    name="name"
+                                    onChange={e => this.onTodoChange(e)}
+                                />
+
+                            </div>
+                            
+                            <br/>
 
                             <h4>Username:</h4>
                             <input
                                 type="text"
                                 placeholder={"Enter your username..."}
                                 name="username"
+                                onChange={e => this.onTodoChange(e)}
                             />
 
                             <br/><br/>
@@ -111,6 +125,7 @@ export class Login extends Component {
                                 type="text"
                                 placeholder={"Enter your password..."}
                                 name="password"
+                                onChange={e => this.onTodoChange(e)}
                             />
 
                           
