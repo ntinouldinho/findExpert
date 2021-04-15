422842ea3fa59606df179be48acda66dc267cce5
GerasimosLap <gerasimoslapakis@gmail.com>
2021-04-15 09:15:49 +0300

    no grid

--- client/src/components/ProfessionalBlock.js
+++ client/src/components/ProfessionalBlock.js
@@ -6,31 +6,47 @@ const ProfessionalBlock = ({ person }) => {
   return (
     <a href={"/profile/" + person.name + "/56"}>
       <div className="ProfessionalBlock">
-          <div>
-            <img
-              alt="profile pic"
-              className="ProfilePic"
-              src={"https://www.ecpi.edu/sites/default/files/CIV%20Sept%2026.png"}
-            />
-          </div>
-
-          <div className="Rating">
+        <div className="leftside" style={{ float: "left" }}>
+          
+          <span class="caption">
+            {person.name} {person.surname} Profession: {person.job}
             <Rating rating={"4"} count={5} />
-          </div>
-
-          <div className="namesurname">
-              {person.name} {person.surname} Profession: {person.job}<br></br>
-          </div>
-
-          <div className="info">
-            m Ipsum. um passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            sum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lore
-            sum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lore
-            sum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lore
+          </span>
+          <img
+            alt="profile pic"
             
-          </div>
+            src={"https://www.ecpi.edu/sites/default/files/CIV%20Sept%2026.png"}
+            width="150px"
+            height="120px"
+          />
         </div>
-      
+        <div className="info">
+          m Ipsum. um passages, and more recently with desktop publishing
+          software like Aldus PageMaker including versions of Lorem Ipsum. sum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lore sum passages, and more
+          recently with desktop publishing software like Aldus PageMaker
+          including versions of Lore sum passages, and more recently with
+          desktop publishing software like Aldus PageMaker including versions of
+          Lore
+          m Ipsum. um passages, and more recently with desktop publishing
+          software like Aldus PageMaker including versions of Lorem Ipsum. sum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lore sum passages, and more
+          recently with desktop publishing software like Aldus PageMaker
+          including versions of Lore sum passages, and more recently with
+          desktop publishing software like Aldus PageMaker including versions of
+          Lore
+          m Ipsum. um passages, and more recently with desktop publishing
+          software like Aldus PageMaker including versions of Lorem Ipsum. sum
+          passages, and more recently with desktop publishing software like
+          Aldus PageMaker including versions of Lore sum passages, and more
+          recently with desktop publishing software like Aldus PageMaker
+          including versions of Lore sum passages, and more recently with
+          desktop publishing software like Aldus PageMaker including versions of
+          Lore
+        </div>
+      </div>
     </a>
   );
 };
