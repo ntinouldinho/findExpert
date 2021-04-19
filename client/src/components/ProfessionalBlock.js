80584651c63f03d515b110a794e2831dec873b30
GerasimosLap <gerasimoslapakis@gmail.com>
2021-04-19 18:05:56 +0300

    Prof Block

--- client/src/components/ProfessionalBlock.js
+++ client/src/components/ProfessionalBlock.js
@@ -5,23 +5,22 @@ import Rating from "react-star-review";
 const ProfessionalBlock = ({ person }) => {
   return (
     <div className="ProfessionalBlock">
-      <a href={"/profile/" + person.name + "/56"}>
+      <a href={"/profile/" + person.name +"/" + person.id}>
         <div className="leftside" style={{ float: "left" }}>
           <img
-            alt="profile pic"
+            alt="Expert Profile Picture"
             className="ProfilePic"
-            src={"https://www.ecpi.edu/sites/default/files/CIV%20Sept%2026.png"}
+            src={person.url}
           />
           <span class="caption">
             {person.name}
             <br></br>
             {person.job}
-            <Rating rating={"4"} count={5} />
+            <Rating rating={person.rating} count={5} />
           </span>
         </div>
         <div className="info">
-          I have been working as plumber 18 years.
-          I am an electrician and I love my work. 
+          {person.info}
         </div>
       </a>
     </div>
--- client/src/routes/Results.js
+++ client/src/routes/Results.js
@@ -12,10 +12,11 @@ export class Results extends Component {
     super(props);
     this.state = {
       professionals: [
-        { job: "Plumber", name: "Kostas Iliopoulos"},
-        { job: "Electrician", name: "Ioannis Argyropoulos"},
-        { job: "Mechanic", name: "Panagiotis Kareas"},
-        { job: "Glazier", name: "Iasonas Mpraxamis"}
+        { job: "Plumber", name: "Kostas Iliopoulos", info: "I love my job", rating: "3.2", id: "666", url: "https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/plumber.jpg?alt=media&token=9f739649-cad2-4582-b045-9de224f7bb0b"},
+        { job: "Electrician", name: "Ioannis Argyropoulos", info: "Book an appointment with me and you won't loose!", rating: "1.5", id: "123", url:"https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/electrician1.jpg?alt=media&token=9fef9a8e-c73a-488a-8772-7f888b2906bb"},
+        { job: "Mechanic", name: "Panagiotis Kareas", info: "I have been a Mechanic all my life and it is really cool!", rating: "4.2", id: "420", url: "https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/mechanic.jpg?alt=media&token=fae310d5-9114-4114-a810-738da31cd7c6"},
+        { job: "Glazier", name: "Iasonas Mpraxamis", info: "Best Glazier in town!", rating: "2.3", id: "023", url: "https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/glazizer.jpg?alt=media&token=a8cfb339-480a-48e4-9000-551fe5206576"},
+        { job: "Electrician", name: "Vasilis Voulas", info: "Reputable Electrician, all jobs can be done", rating: "5", id: "555",url:"https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/electrician2.jpg?alt=media&token=4488e618-0cb1-416c-a6dc-ed6a0ba19dd7"}
       ],
       search: props.match.params.search,
     };
