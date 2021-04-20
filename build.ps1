d23c8805df30acae2f844a86bbe490a11549b32e
GerasimosLap <gerasimoslapakis@gmail.com>
2021-04-20 18:08:11 +0300

    services add

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 Write-Host "Congratulations! Now we run!"
-"yarn install" | cmd
+#"yarn install" | cmd
 $PATH = Get-Location
 $server = "PowerShell.exe", "-NoExit", "-Command", "& { `$Host.UI.RawUI.WindowTitle='FindExpertServer'\; `$Path\; Push-Location $Path\; node server.js}"
 $run = "PowerShell.exe", "-NoExit", "-Command", "& {`$Host.UI.RawUI.WindowTitle='FindExpertRun'\;`$Path\;Push-Location $Path\client\;yarn start}"
