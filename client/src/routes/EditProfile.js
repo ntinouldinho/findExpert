3f632e7c643bf1b02daa2576b41763d1edf7634f
GerasimosLap <gerasimoslapakis@gmail.com>
2021-04-20 13:28:40 +0300

    Edit Profile

--- client/src/routes/EditProfile.js
+++ client/src/routes/EditProfile.js
@@ -1,83 +1,92 @@
-import React, { Component } from 'react';
-import '../CSS/Profile.css';
-import '../CSS/EditProfile.css';
-import Rating from 'react-star-review';
+import React, { Component } from "react";
+import "../CSS/Profile.css";
+import "../CSS/EditProfile.css";
+import Rating from "react-star-review";
 import { Header } from "../components/Header";
-import { EditorState, convertToRaw } from 'draft-js';
-import { Editor } from 'react-draft-wysiwyg';
-import draftToHtml from 'draftjs-to-html';
+import { EditorState, convertToRaw } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import draftToHtml from "draftjs-to-html";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-export class EditProfile extends Component { 
-    constructor(props){
-        super(props);
-        this.state = {
-            name: "Onoma",
-            profession: "Epaggelma",
-            photo: "Fwtografia",
-            reviews: ["4", " 5"],
-            services: ["$$ ", " $$"],
-            cv: "My CV",
-            about: "About me",
-            stars: "4.2",
-            appointment: "appointment",
-            editorState: EditorState.createEmpty(),
-        };
-        this.onEditorStateChange=this.onEditorStateChange.bind(this);
-    }
-    onEditorStateChange(editorState) {
-        this.setState({
-          editorState:editorState
-        });
-      };
+export class EditProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Kostas Iliopoulos",
+      profession: "Plumber",
+      photo: "Fwtografia",
+      reviews: ["4", " 5"],
+      services: ["$$ ", " $$"],
+      cv: "My CV",
+      about: "About me",
+      stars: "4.2",
+      appointment: "appointment",
+      editorState: EditorState.createEmpty(),
+    };
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
+  }
+  onEditorStateChange(editorState) {
+    this.setState({
+      editorState: editorState,
+    });
+  }
 
-    render(){
-        const editorState  = this.state.editorState;
-        return(
-            <div> 
-                <Header/>
-                
-                <div className="core">
-                    <div className = "grid-container">
-                        <div className="picture">
-                            <img src={"https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/CategoryTechnician.jpg?alt=media&token=3bc1738b-8de1-4b2f-95d9-ce5783d71823"} alt="Profile" height="100" width="200" id="profpic"/>
-                            {/* <img src={logo} alt="Profile Picture" height="100" width="200" id="profpic"/> */}
-                            <input type="file"></input>
-                        </div>
+  render() {
+    const editorState = this.state.editorState;
+    return (
+      <div>
+        <Header />
 
-                        <div className="info">
-                            {this.state.name}<br></br>
-                            {this.state.profession}<br></br>
-                            {/* <Rating rating={this.state.stars} count={5}/><br></br> */}
-                        </div>
+        <div className="core">
+          <div className="grid-container-edit">
+            <img
+              className="picture"
+              src={
+                "https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/plumber.jpg?alt=media&token=9f739649-cad2-4582-b045-9de224f7bb0b"
+              }
+              alt="Profile"
+              height="100"
+              width="200"
+              id="profpic"
+            />
 
-                        <div className="aboutme">
-                            <input defaultValue={this.state.about}/>
-                        </div>
+            
+            <input className="AddPicture" type="file" name="asda"></input>
 
-                        <div className="services">
-                            <input placeholder="What are your services" defaultValue={this.state.services}/><br></br>
-                        </div>
+            <input className="AddName" type="text" placeholder="Full Name" defaultValue={this.state.name}></input>
+              
+            <input className="AddProfession" type="text" placeholder="Profession" defaultValue={this.state.profession}></input>
 
-                        <div className="cv editor">
-                                <Editor
-                                editorState={editorState}
-                                wrapperClassName="demo-wrapper"
-                                editorClassName="demo-editor"
-                                onEditorStateChange={this.onEditorStateChange}
-                                />
-                                <textarea
-                                disabled
-                                value={draftToHtml(convertToRaw(editorState.getCurrentContent()))}
-                                />
-                        </div>
+            <div className="EditAboutMe">
+              <Editor
+                editorState={editorState}
+                wrapperClassName="demo-wrapper"
+                editorClassName="demo-editor"
+                onEditorStateChange={this.onEditorStateChange}
+              />
+              <textarea
+                disabled
+                value={draftToHtml(
+                  convertToRaw(editorState.getCurrentContent())
+                )}
+              />
+            </div>
 
-                        <div className="reviews">
-                                {this.state.reviews}<br></br>
-                        </div>
-                    </div>
-                </div>
-            </div>   
-        )
-    }
+            <div className="services">
+              <input
+                placeholder="What are your services"
+                defaultValue={this.state.services}
+              />
+              <br></br>
+            </div>
+
+            {/* <div className="reviews">
+              {this.state.reviews}
+              <br></br>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
