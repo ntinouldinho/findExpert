3e44ba8efa8bc36a1983e7acb1290bdde775930f
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2021-05-22 17:39:47 +0300

    replaced id with hashed firebase user value

--- client/src/routes/Results.js
+++ client/src/routes/Results.js
@@ -25,7 +25,7 @@ export class Results extends Component {
           info:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis tincidunt elit. Morbi eget elit id lorem feugiat pharetra. Cras nec tortor ac ante volutpat hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at pretium ante. Morbi elit felis, sollicitudin iaculis malesuada id, tincidunt in turpis. Phasellus aliquet dapibus enim eget bibendum. Pellentesque molestie nulla purus, tincidunt lacinia orci aliquet at. Mauris sit amet rhoncus felis, quis aliquam eros. Vivamus lacinia dignissim malesuada. Vestibulum efficitur, orci in fringilla rhoncus, tortor ante luctus turpis, id fermentum libero dui ac libero.",
           rating: "3.2",
-          id: "666",
+          id: "HVp43gujF3Ssoor8t4hGN5jA1w33",
           url:
             "https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/plumber.jpg?alt=media&token=9f739649-cad2-4582-b045-9de224f7bb0b",
         },
@@ -35,7 +35,7 @@ export class Results extends Component {
           info:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis tincidunt elit. Morbi eget elit id lorem feugiat pharetra. Cras nec tortor ac ante volutpat hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at pretium ante. Morbi elit felis, sollicitudin iaculis malesuada id, tincidunt in turpis. Phasellus aliquet dapibus enim eget bibendum. Pellentesque molestie nulla purus, tincidunt lacinia orci aliquet at. Mauris sit amet rhoncus felis, quis aliquam eros. Vivamus lacinia dignissim malesuada. Vestibulum efficitur, orci in fringilla rhoncus, tortor ante luctus turpis, id fermentum libero dui ac libero.",
           rating: "1.5",
-          id: "123",
+          id: "HVp43gujF3Ssoor8t4hGN5jA1w33",
           url:
             "https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/electrician1.jpg?alt=media&token=9fef9a8e-c73a-488a-8772-7f888b2906bb",
         },
@@ -45,7 +45,7 @@ export class Results extends Component {
           info:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis tincidunt elit. Morbi eget elit id lorem feugiat pharetra. Cras nec tortor ac ante volutpat hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at pretium ante. Morbi elit felis, sollicitudin iaculis malesuada id, tincidunt in turpis. Phasellus aliquet dapibus enim eget bibendum. Pellentesque molestie nulla purus, tincidunt lacinia orci aliquet at. Mauris sit amet rhoncus felis, quis aliquam eros. Vivamus lacinia dignissim malesuada. Vestibulum efficitur, orci in fringilla rhoncus, tortor ante luctus turpis, id fermentum libero dui ac libero.",
           rating: "4.2",
-          id: "420",
+          id: "HVp43gujF3Ssoor8t4hGN5jA1w33",
           url:
             "https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/Constructor.jpg?alt=media&token=2be478bf-e12b-4a7c-a9c3-c9d43fd16e20",
         },
@@ -55,7 +55,7 @@ export class Results extends Component {
           info:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis tincidunt elit. Morbi eget elit id lorem feugiat pharetra. Cras nec tortor ac ante volutpat hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at pretium ante. Morbi elit felis, sollicitudin iaculis malesuada id, tincidunt in turpis. Phasellus aliquet dapibus enim eget bibendum. Pellentesque molestie nulla purus, tincidunt lacinia orci aliquet at. Mauris sit amet rhoncus felis, quis aliquam eros. Vivamus lacinia dignissim malesuada. Vestibulum efficitur, orci in fringilla rhoncus, tortor ante luctus turpis, id fermentum libero dui ac libero.",
           rating: "2.3",
-          id: "023",
+          id: "HVp43gujF3Ssoor8t4hGN5jA1w33",
           url:
             "https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/glazizer.jpg?alt=media&token=a8cfb339-480a-48e4-9000-551fe5206576",
         },
@@ -65,7 +65,7 @@ export class Results extends Component {
           info:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis tincidunt elit. Morbi eget elit id lorem feugiat pharetra. Cras nec tortor ac ante volutpat hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at pretium ante. Morbi elit felis, sollicitudin iaculis malesuada id, tincidunt in turpis. Phasellus aliquet dapibus enim eget bibendum. Pellentesque molestie nulla purus, tincidunt lacinia orci aliquet at. Mauris sit amet rhoncus felis, quis aliquam eros. Vivamus lacinia dignissim malesuada. Vestibulum efficitur, orci in fringilla rhoncus, tortor ante luctus turpis, id fermentum libero dui ac libero.",
           rating: "5",
-          id: "555",
+          id: "HVp43gujF3Ssoor8t4hGN5jA1w33",
           url:
             "https://firebasestorage.googleapis.com/v0/b/professionall.appspot.com/o/electrician2.jpg?alt=media&token=4488e618-0cb1-416c-a6dc-ed6a0ba19dd7",
         },
