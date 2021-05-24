5360f539a27ca9146a50ae55cd21aa010574ccc1
Bill Ballas <ballas@programmer.net>
2021-05-24 12:56:55 +0300

    opacity fix f heroku edit btn

--- client/src/routes/EditProfile.js
+++ client/src/routes/EditProfile.js
@@ -6,35 +6,99 @@ import { Header } from "../components/Header";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import { EditorState, ContentState, convertToRaw, convertFromHTML } from 'draft-js';
+import {
+  EditorState,
+  ContentState,
+  convertToRaw,
+  convertFromHTML,
+} from "draft-js";
 import ListAdder from "../components/ListAdder";
+import Button from "react-bootstrap/Button";
+// import console from "node:console";
 
 export class EditProfile extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: "Kostas Iliopoulos",
-      profession: "Plumber",
-      photo: "Fwtografia",
-      reviews: ["4", " 5"],
-      services: ["$$ ", " $$"],
-      cv: "My CV",
-      about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis tincidunt elit. Morbi eget elit id lorem feugiat pharetra. Cras nec tortor ac ante volutpat hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at pretium ante. Morbi elit felis, sollicitudin iaculis malesuada id, tincidunt in turpis. Phasellus aliquet dapibus enim eget bibendum. Pellentesque molestie nulla purus, tincidunt lacinia orci aliquet at. Mauris sit amet rhoncus felis, quis aliquam eros. Vivamus lacinia dignissim malesuada. Vestibulum efficitur, orci in fringilla rhoncus, tortor ante luctus turpis, id fermentum libero dui ac libero. ",
-      stars: "4.2",
-      appointment: "appointment",
-      editorState: EditorState.createWithContent(
-        ContentState.createFromBlockArray(
-          convertFromHTML('<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis tincidunt elit. Morbi eget elit id lorem feugiat pharetra. Cras nec tortor ac ante volutpat hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at pretium ante. Morbi elit felis, sollicitudin iaculis malesuada id, tincidunt in turpis. Phasellus aliquet dapibus enim eget bibendum. Pellentesque molestie nulla purus, tincidunt lacinia orci aliquet at. Mauris sit amet rhoncus felis, quis aliquam eros. Vivamus lacinia dignissim malesuada. Vestibulum efficitur, orci in fringilla rhoncus, tortor ante luctus turpis, id fermentum libero dui ac libero. </p>')
-        )
-      ),
-    };
+      this.state = {
+        id: "HVp43gujF3Ssoor8t4hGN5jA1w33",
+        name: "Kostas Iliopoulos",
+        profession: "Plumber",
+        photo: "Fwtografia",
+        reviews: ["4", " 5"], //nonCustom
+        services: ["$$ ", " $$"], //nonCustom
+        cv: "My CV",
+        about:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis tincidunt elit. Morbi eget elit id lorem feugiat pharetra. Cras nec tortor ac ante volutpat hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at pretium ante. Morbi elit felis, sollicitudin iaculis malesuada id, tincidunt in turpis. Phasellus aliquet dapibus enim eget bibendum. Pellentesque molestie nulla purus, tincidunt lacinia orci aliquet at. Mauris sit amet rhoncus felis, quis aliquam eros. Vivamus lacinia dignissim malesuada. Vestibulum efficitur, orci in fringilla rhoncus, tortor ante luctus turpis, id fermentum libero dui ac libero. ",
+        stars: "4.2", //nonCustom
+        appointment: "appointment", //nonCustom
+        editorState: EditorState.createWithContent(
+          ContentState.createFromBlockArray(
+            convertFromHTML(
+              "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis tincidunt elit. Morbi eget elit id lorem feugiat pharetra. Cras nec tortor ac ante volutpat hendrerit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at pretium ante. Morbi elit felis, sollicitudin iaculis malesuada id, tincidunt in turpis. Phasellus aliquet dapibus enim eget bibendum. Pellentesque molestie nulla purus, tincidunt lacinia orci aliquet at. Mauris sit amet rhoncus felis, quis aliquam eros. Vivamus lacinia dignissim malesuada. Vestibulum efficitur, orci in fringilla rhoncus, tortor ante luctus turpis, id fermentum libero dui ac libero. </p>"
+            )
+          )
+        ),
+      };
     this.onEditorStateChange = this.onEditorStateChange.bind(this);
   }
+
+  async componentDidMount() {
+    try {
+      const response = await fetch(`/api/decode`);
+      const jsan = await response.json();
+      const user = jsan.user;
+      const data = await fetch(`/api/user/get?user=${user}`);
+        const json = await data.json();
+        console.log(jsan.user);
+        this.setState({
+          id:jsan.user,
+        name: json.name + " " + json.surname,
+        profession: json.profession,
+        about: json.about,
+        photo: json.photo,
+        services: json.services,
+        cv: json.cv,
+      });
+    } catch (error) {}
+  }
+
   onEditorStateChange(editorState) {
     this.setState({
       editorState: editorState,
     });
   }
+
+  handleUpdate = async (e) => {     //prepei na ta apothikeyei prwta kapou
+    e.preventDefault();
+    fetch("/api/expert/edit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // name: json.name + " " + json.surname,
+        profession: this.state.profession,
+        about: this.state.about,
+        photo: this.state.photo,
+        services: this.state.services,
+        cv: this.state.cv,
+      }),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          this.props.history.push(
+            "/profile/" + this.state.name + "/" + this.state.id
+          );
+        } else {
+          const error = new Error(res.error);
+          throw error;
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        alert("Error logging in please try again");
+      });
+  };
 
   render() {
     const editorState = this.state.editorState;
@@ -55,22 +119,33 @@ export class EditProfile extends Component {
               id="profpic"
             />
 
-            
             <input className="AddPicture" type="file" name="asda"></input>
 
             <div className="AddName">
-            <label for="name">Name: </label>
-            <input id="name"  type="text" placeholder="Full Name" defaultValue={this.state.name}></input>
+              <label for="name">Name: </label>
+              <input
+                id="name"
+                type="text"
+                placeholder="Full Name"
+                defaultValue={this.state.name}
+              ></input>
             </div>
 
             <div className="AddProfession">
-            <label for="profession">Profession: </label>
-            <input id="profession"  type="text" placeholder="Profession" defaultValue={this.state.profession}></input>
+              <label for="profession">Profession: </label>
+              <input
+                id="profession"
+                type="text"
+                placeholder="Profession"
+                defaultValue={this.state.profession}
+              ></input>
             </div>
 
-            <Rating className="RatingOnEdit" rating={this.state.stars} count={5} />
-            
-            
+            <Rating
+              className="RatingOnEdit"
+              rating={this.state.stars}
+              count={5}
+            />
 
             <div className="EditAboutMe">
               <Editor
@@ -80,7 +155,6 @@ export class EditProfile extends Component {
                 onEditorStateChange={this.onEditorStateChange}
               />
               <textarea
-              
                 disabled
                 value={draftToHtml(
                   convertToRaw(editorState.getCurrentContent())
@@ -89,13 +163,13 @@ export class EditProfile extends Component {
             </div>
 
             <div className="AddServices">
-                <h1>Services</h1>
-                <ListAdder />
+              <h1>Services</h1>
+              <ListAdder />
             </div>
 
             <div className="AddCV">
-                <h1>My CV</h1>
-                <ListAdder />
+              <h1>My CV</h1>
+              <ListAdder />
             </div>
 
             <div className="reviews">
@@ -103,6 +177,9 @@ export class EditProfile extends Component {
               <br></br>
             </div>
           </div>
+          <Button id="updatebtn" onClick={this.handleUpdate} variant="success">
+            Update Profile
+          </Button>
         </div>
       </div>
     );
