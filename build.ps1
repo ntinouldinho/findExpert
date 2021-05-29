31e0a907c8752435584205ac670c64bb3e9d68df
ntinouldinho <37417801+ntinouldinho@users.noreply.github.com>
2021-05-29 09:44:39 +0300

    add nodemon

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 Write-Host "Welcome to findExpert!"
 # "yarn install" | cmd
 $PATH = Get-Location
-$server = "PowerShell.exe", "-NoExit", "-Command", "& { `$Host.UI.RawUI.WindowTitle='FindExpertServer'\; `$Path\; Push-Location $Path\; node server.js}"
+$server = "PowerShell.exe", "-NoExit", "-Command", "& { `$Host.UI.RawUI.WindowTitle='FindExpertServer'\; `$Path\; Push-Location $Path\; nodemon server.js}"
 $run = "PowerShell.exe", "-NoExit", "-Command", "& {`$Host.UI.RawUI.WindowTitle='FindExpertRun'\;`$Path\;Push-Location $Path\client\;yarn start}"
 $build = $server + ';' + $run
 Start-Process wt.exe -ArgumentList $build
