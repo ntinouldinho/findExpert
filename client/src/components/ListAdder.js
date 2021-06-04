d8561b68130302fc97e1e044473dde0183acdac7
GerasimosLap <gerasimoslapakis@gmail.com>
2021-06-04 20:17:38 +0300

    Map

--- client/src/components/ListAdder.js
+++ client/src/components/ListAdder.js
@@ -4,9 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import console from "node:console";
 
 function ListAdder(props) {
-    const [fields, setFields] = useState(props.value);
-    
-
+  const [fields, setFields] = useState(props.value);
 
   function handleChange(i, event) {
     const values = [...fields];
@@ -25,36 +23,59 @@ function ListAdder(props) {
     values.splice(i, 1);
     setFields(values);
   }
-
+  // console.log(props.value)
+  {Object.keys(props.value).map((e, i) => {
+      // console.log(e)
+      // console.log(props.value[e])
+  })}
   return (
     <table className="ListAdder">
-     <button type="button" onClick={() => handleAdd()}>
-        <FontAwesomeIcon icon={faPlusCircle} />
-    </button>
-
-    {props.value.map((name, id) => {
-        console.log(name);
-        console.log(id);
-        return (
-          <tr key={`${name}-${id}`}>
-            <input
-              type="text"
-              placeholder="Enter text"
-              value={name || "Add your " + props.type}
-              onChange={(e) => handleChange(id, e)}
-            />
-            <input
-              type="number"
-              placeholder="€"
-              value={name.value || 0}
-            />
-            <button type="button" onClick={() => handleRemove(id)}>
-              <FontAwesomeIcon icon={faTrashAlt} />
+      <thead>
+        <tr>
+          <th>Service</th>
+          <th>Price</th>
+          <th>
+            <button type="button" onClick={() => handleAdd()}>
+              <FontAwesomeIcon icon={faPlusCircle} />
             </button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.keys(props.value).map((e, i) => {
+          <tr>
+            <td>123</td>
+            <td>123</td>
+            <td>
+              <button type="button" onClick={() => handleRemove(i)}>
+                <FontAwesomeIcon icon={faTrashAlt} />
+              </button>
+            </td>
           </tr>
-        );
-      })}
+        })}
+      </tbody>
     </table>
   );
+
+  //     {props.value.map((name, id) => {
+  //       console.log(name);
+  //       console.log(id);
+  //       return (
+  //         <tr key={`${name}-${id}`}>
+  //           <input
+  //             type="text"
+  //             placeholder="Enter text"
+  //             value={name || "Add your " + props.type}
+  //             onChange={(e) => handleChange(id, e)}
+  //           />
+  //           <input type="number" placeholder="€" value={name.value || 0} />
+  //           <button type="button" onClick={() => handleRemove(id)}>
+  //             <FontAwesomeIcon icon={faTrashAlt} />
+  //           </button>
+  //         </tr>
+  //       );
+  //     })}
+  //
+  // );
 }
 export default ListAdder;
